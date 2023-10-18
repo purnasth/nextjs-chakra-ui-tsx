bc5afb22214f6c1e019266de47168cbcacb759d0
Purna Shrestha <crush9918@gmail.com>
2023-10-18 11:09:26 +0545

    Create a carousel with captions

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 import Footer from "@/components/Footer";
 import Contact from "@/components/Contact";
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Carousel />
       <Features />
       <Testimonials />
       <Contact />

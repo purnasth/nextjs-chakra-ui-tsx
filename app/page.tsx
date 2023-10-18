bf3104e1e5032c3517b01178bcdb67b2965d8387
Purna Shrestha <crush9918@gmail.com>
2023-10-18 10:46:20 +0545

    Create a testimonial component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import HeroSection from "@/components/HeroSection";
 import Features from "@/components/Features";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Features />
+      <Testimonials />
     </>
   );
 }

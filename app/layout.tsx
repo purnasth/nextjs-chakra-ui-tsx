eb2aa7df799f8091e5809fe5ae6154491b4fb1f1
Purna Shrestha <crush9918@gmail.com>
2023-10-19 11:13:12 +0545

    Update the preloader with an typeof window if

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,18 @@
 "use client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
-import Preloader from "@/components/Preloader"; // Import the Preloader component
+import Preloader from "@/components/Preloader";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2500);
-  }, []);
-
   return (
     <html lang="en">
       <body>
         <ChakraProvider>
-          {loading ? <Preloader /> : null}
+          <Preloader />
           <Navbar />
           {children}
         </ChakraProvider>
